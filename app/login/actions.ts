5d157941aa389d19fc1ca8e268fb05883d60d2d2
Filip <orestafilip@gmail.com>
2024-08-28 20:33:20 +0200

    upd

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -20,6 +20,7 @@ export async function login(formData: FormData) {
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
+    console.log("Error logging in:", error);
     redirect("/error");
   }
 
