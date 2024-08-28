1b3e4e155e98d85df92c4d8987e8fe705d94bfe6
Filip <orestafilip@gmail.com>
2024-08-28 20:22:16 +0200

    upd

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,6 +1,16 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
+  console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("Supabase Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ) {
+    throw new Error("Supabase URL and Key must be provided!");
+  }
+
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
