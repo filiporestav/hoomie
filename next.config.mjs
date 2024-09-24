6abaf4f1750b6618a1ab10d71b341a2b334c7fe1
kolumbuslindh <164384780+kolumbuslindh@users.noreply.github.com>
2024-09-24 11:51:05 +0200

    adlist working!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['nojlqqfvtuiolmpizvqf.supabase.co'],
+    },
+  };
+  
+  export default nextConfig;
+  
